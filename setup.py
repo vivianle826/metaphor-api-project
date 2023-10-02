1f0fc2c22b613c5741fb36e46665d39d86dcc80e
Vivian Le <vivianle826@berkeley.edu>
2023-10-02 03:24:18 -0700

    commit with extra files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     author="Vivian Le",
     install_requires=[
-        "Flask==2.0.3", 
+        "Flask", 
         "Jinja2==2.11.3", 
         "metaphor-python==0.1.16", 
         "requests==2.27.1"],
